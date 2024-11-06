f2a57ddc7e87b37ce5f7431f0ff228e2d91081a4
Baptiste-Verdier <baptiste.verdier@artfx.fr>
2024-11-06 12:01:01 +0100

    started respawn death

--- Assets/Scripts/Marie/Checkpoint.cs
+++ Assets/Scripts/Marie/Checkpoint.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+   static public GameObject firstFlag;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             collision.GetComponent<PlayerMovement>().ChangeCheckpoint(this);
         }
+        
     }
 }
